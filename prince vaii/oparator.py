f7e8627477cacacb665c7b7a093c4a76ebd00f55
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-11-08 22:07:30 +0600

    oparator

---
+++ prince vaii/oparator.py
@@ -0,0 +1,13 @@
+#Types of Operators & Arithmatic Operators in Python
+
+#Arithmatic operator: + - * /
+#Assingment opetator: -= += *= /=
+#Compaparison operator: == < <= > >= !
+#logical operator: and or not
+
+a = 10
+b = 30
+print(a + b)
+print(a-b)
+print(a*b)
+print(a/b)
---
+++ prince vaii/var-lekhar-niyom.py
@@ -0,0 +1,21 @@
+#4 naming variables & determining data type
+
+
+#data type in python 
+
+#data type 4 main
+myName = "kawsar kabir" #its str
+myMarks = 70 #its intiger
+myAnotherMarks = 40.5 #its float
+isSingleMe = True #its bolean
+print (type(myName))
+print(type(myMarks))
+print(type(myAnotherMarks))
+print(type(isSingleMe))
+
+
+
+
+#1 নামের প্রথমে নাম্বার দিয়া লেখা যাবে নাহ
+#২ নামের মাঝখানে গ্যাপ থাকা যাবে না
+#৩ 
