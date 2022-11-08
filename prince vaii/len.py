d2c6a4d800e180c7d738c96a309763dcd68c5dbf
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-11-09 01:36:31 +0600

    len

---
+++ prince vaii/len.py
@@ -0,0 +1,3 @@
+rochona ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis fugiat voluptas cumque ea blanditiis accusamus veniam, consequuntur quae amet. Deleniti culpa sint quia voluptatem incidunt omnis, ducimus ab. Suscipit, sequi."
+print(len(rochona))
+print(rochona.endswith("."))
