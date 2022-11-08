d71912059ec3dbad2f1ccedafe7b663932dfccbf
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-11-08 21:39:09 +0600

    variable data type

---
+++ prince vaii/var-datatype.py
@@ -0,0 +1,8 @@
+#data type in python 
+
+#data type 4 main
+myName = "kawsar kabir" #its str
+myMarks = 70 #its intiger
+myAnotherMarks = 40.5 #its float
+isSingleMe = True #its bolean
+print (myName , myMarks, myAnotherMarks, isSingleMe) 
