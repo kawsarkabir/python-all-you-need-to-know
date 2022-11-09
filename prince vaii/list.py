0e1348dd56af424f0cb5e1d0e6405da9e843584d
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-11-10 01:13:48 +0600

    few list items in python

---
+++ prince vaii/list.py
@@ -0,0 +1,13 @@
+list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#element add kora 
+list.append(40)
+print(list)
+
+#fast a element a add korar niyom
+list.insert(0, 70)
+print(list)
+
+#element remove korar niyom
+list.remove(70)
+print(list)
