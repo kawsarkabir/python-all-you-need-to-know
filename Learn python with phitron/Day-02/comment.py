5310ad04b73c921b355413f1cf1d971e5fc3226d
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-19 12:31:39 +0600

    python oparator

---
+++ Learn python with phitron/Day-02/comment.py
@@ -0,0 +1,6 @@
+# this is a comment // single line comment => ctrl + /
+""" alt + shift + A
+this is 
+multiple line 
+comment
+"""
---
+++ Learn python with phitron/Day-02/oparator.py
@@ -0,0 +1,22 @@
+#Python Arithmetic Operators
+# +, -, * /, %
+x = 15;
+y = 2;
+print(x + y) #sumation
+print(x - y) #sub
+print(x * y) #multiple 
+print(x / y) #divition
+print(x % y) #modules
+print(x // y) # Floor Divition
+print(x ** y) # Exponentiationw
+
+# Python Comparison Operators
+""" ==, !=, >, <, >=, <= """
+a = 10;
+b = 10;
+print(a > b); # greater then 
+print(a < b); #less then
+print(a >= b); # greater then or equal to
+print(a <= b); # less then or equal to
+print(a != b); #not equal to 
+print(a == b); # equal to 
