df7e7f50ed1949d61fcebdd3ebfad3ee158b1362
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-19 16:58:37 +0600

    grading problem solving

---
+++ problem solving with python/Day-03/gradeing.py
@@ -0,0 +1,9 @@
+marks = int (input('Enter Your Marks: '))
+if marks > 90:
+    print('Your Grade is A')
+elif marks > 80 and marks <= 90:
+    print(' Your Grade is B')
+elif marks >= 60 and marks <= 80:
+    print(' Your Grade is C')
+else:
+    print(' Your Grade is D')    
