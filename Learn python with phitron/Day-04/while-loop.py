48c8d19ae8d7539af277d0fa07fe4e0fcf0b76e1
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-20 01:37:37 +0600

    break , continue in for while loop

---
+++ Learn python with phitron/Day-04/while-loop.py
@@ -0,0 +1,30 @@
+a = 0;
+while a<=10:
+    a = a + 1
+    print(a)
+
+# break keyword in for and while loop
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+# same jinis in whle loop
+while a <= 5:
+    a = a + 1
+    if a == 5:
+        break
+    print(a)
+
+# continused
+for i in range(10):
+ if i == 5:
+     continue
+ print(i)
+
+y = 1
+while y <=10:
+    y = y + 1
+    if y == 5:
+        continue
+    print(y)
