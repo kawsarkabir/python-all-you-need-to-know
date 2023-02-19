ec347e255a51e7b76a1bec48ca2e5291953295af
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-19 16:48:43 +0600

    problem 03 done

---
+++ problem solving with python/Day-03/evenOddNumber.py
@@ -0,0 +1,8 @@
+""" === problem 03 ===
+write a program using conditional statement whwther a number is even or odd
+"""
+num = int(input('Enter the Num: '));
+if num % 2 == 0:
+    print(num, 'is Even Nubmer')
+else:
+    print(num, 'is odd number')
