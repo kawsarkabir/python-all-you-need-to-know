8574c5537cb4a57842bbbe2a6a58f3adfc7dd761
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-19 13:42:23 +0600

    day 2 done with conditional

---
+++ Learn python with phitron/Day-02/typecasting.py
@@ -0,0 +1,13 @@
+# integer funciton 
+age = int (input())
+if age >= 18:
+    print('you are adult')
+else:
+    print('you are child') 
+
+weight = float(input())
+if weight >= 48.89:
+    print('thik ase tumar weight')
+else:
+    print('na its wrong')
+print(type(weight))    
