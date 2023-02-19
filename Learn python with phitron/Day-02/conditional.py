f1d40e5dcdcb16a316dab36ec6c333fe2f289cd5
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-19 13:06:03 +0600

    if , else, elif

---
+++ Learn python with phitron/Day-02/conditional.py
@@ -0,0 +1,19 @@
+# 2.2 - Introduction to Conditional Statement
+""" what is conditional statement  ?
+=> conditional statements on programming are used to make dicition base on the condition 
+"""
+
+rain = False
+if rain==True: 
+    print('scl a jabo')
+else:
+    print('jabo na')    
+          
+# >=
+age = 17
+if age >= 18:
+    print('tumi license paba')
+else:
+    print('tumi 18-')
+
+
---
+++ Learn python with phitron/Day-02/logical-oparator.py
@@ -0,0 +1,22 @@
+# Ptyhon Logical Operaters
+# and 
+# or
+#not
+
+print(10 - 4 == 6 and 10 - 5 == 15)
+print(10 - 4 == 6 and 10 - 5 == 15)
+
+print(10 - 4 == 6 or 10 - 5 == 15)
+print(10 - 4 == 6 or 10 - 5 == 15)
+
+# print(10 - 4 == 6  10 - 5 == 15)
+
+print(not (10 - 4 == 6))
+
+marks = 75
+if marks>=90 and marks <= 100:
+    print('tumi candy box paba')
+elif marks>=80 and marks<=89:
+    print('tumi akta paba paba')
+else:
+    print('kichui paba na')
