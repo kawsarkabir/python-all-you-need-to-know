4755d7e0e159d3b87262c703c94504107b621bce
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-19 16:21:46 +0600

    problem 01 sloved

---
+++ problem solving with python/Day-03/reacangle.py
@@ -0,0 +1,11 @@
+"""====problem solving 01===
+Take Value and lenght and breadth of a reactangle form user and check if it is square or not."""
+
+a = int (input('Enter the lenght of the recangle'))
+b = int (input('Enter the breadth of the recangle'))
+
+if a == b:
+    print('This is a squre')
+else:
+    print('This is a recangle')
+    
