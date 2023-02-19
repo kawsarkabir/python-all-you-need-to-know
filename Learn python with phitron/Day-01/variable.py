09239bdb9f09f1d7d0b4004f86dc7c55e94324c2
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-19 12:04:45 +0600

    variable

---
+++ Learn python with phitron/Day-01/variable.py
@@ -0,0 +1,22 @@
+"""what is variable ?
+==> variable like conteiners where storeing data, like number , cherecter"""
+
+""" ===Data Type in py=== """
+#no2. string
+x = 'kawsar kabir'
+print(x)
+#no1. number
+a = 10;
+print(a);
+#bolean 
+isSingle = True;
+
+""" 
+jevabe variable namming convension kora jbe na
+"""
+# 1num
+# name sensitive > num = 10, vs Num = 10;
+#recomended => num1
+
+my_list = ['alu', 'piyaz'];
+print(my_list)
